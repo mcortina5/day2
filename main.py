e77dc401639dd9331e7b13899464c1a5eb7a5a85
Cortina <mcortina@cps.k12.il.us>
2024-04-08 13:28:30 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,36 @@
+# # order of execution
+# # in python
+# print("hello world")
+# print("order of execution")
+# #stings are surrounded by quotes
+# #single or double quotes '' or ""
+# #be consistent with the quotes you use
+# print("in python")
+# print("*"*20)
+
+# #variables are used to store data
+# #variables are created when you assign a value to it
+# #variables are case sensitive
+# price = 10 
+#integer variable
+# name = "John" #string variable
+# rating = 4.9 #float variable
+# is_published = True #boolean variable
+# #start all variables with a lowercase letter 
+# #use underscores to seperate words
+# #use camel case if you want to start with a capital letter on the next word
+# playerBulls = "micheal jordan"
+# #Concatentation to join variables in a sentence
+# print(name + " is a basketball player")
+# print(name + " has a rating of" + str(rating))
+# #use the str() function to convert a number to a string
+# #plus sign is used to concatenate strings
+# print("the price of the book is" + str(price))
+# name = input("What is your name? ")
+# age = input("What is your age? ")
+# print("Hello " + name + " you are " + age + " years old")
+
+personName = input("What is your name?")
+favoriteColor = input("What is your favorite color?")
+print(personName + " likes " + favoriteColor)
+
